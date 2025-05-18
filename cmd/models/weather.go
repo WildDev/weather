cfeb26e6400a493e97a9747e756454de16b0b2a9
WildDev <georgy@wilddev.email>
2025-05-19 00:14:32 +0300

    Condition field added

--- cmd/models/weather.go
+++ cmd/models/weather.go
@@ -9,18 +9,45 @@ type Weather struct {
 	Id          string
 	Country     string
 	City        string
-	ValueC      int
-	MinValueC   int
-	MaxValueC   int
-	ValueF      int
-	MinValueF   int
-	MaxValueF   int
+	Now         *WeatherNow
+	Forecast    []*WeatherForecast
 	Timestamp   *time.Time
 	LastUpdated *time.Time
 	Stale       bool
 }
 
+type WeatherNow struct {
+	ValueC    int
+	ValueF    int
+	Condition string
+}
+
+type WeatherForecast struct {
+	Date      string
+	DateEpoch int64
+	Day       *WeatherForecastDay
+}
+
+type WeatherForecastDay struct {
+	MinValueC int
+	MaxValueC int
+	MinValueF int
+	MaxValueF int
+	Condition string
+}
+
 func (w *Weather) String() string {
-	return fmt.Sprintf("Id=%s Country=%s City=%s ValueC=%d MinValueC=%d MaxValueC=%d ValueF=%d MinValueF=%d MaxValueF=%d Timestamp=%v LastUpdated=%v Stale=%v",
-		w.Id, w.Country, w.City, w.ValueC, w.MinValueC, w.MaxValueC, w.ValueF, w.MinValueF, w.MaxValueF, *w.Timestamp, *w.LastUpdated, w.Stale)
+	return fmt.Sprintf("Id=%s Country=%s City=%s Now=(%v) Forecast=%v Timestamp=%v LastUpdated=%v Stale=%v", w.Id, w.Country, w.City, *w.Now, w.Forecast, *w.Timestamp, *w.LastUpdated, w.Stale)
+}
+
+func (w *WeatherNow) String() string {
+	return fmt.Sprintf("ValueC=%d ValueF=%d Condition=%s", w.ValueC, w.ValueF, w.Condition)
+}
+
+func (w *WeatherForecast) String() string {
+	return fmt.Sprintf("Date=%s DateEpoch=%d Day=(%v)", w.Date, w.DateEpoch, *w.Day)
+}
+
+func (w *WeatherForecastDay) String() string {
+	return fmt.Sprintf("MinValueC=%d MaxValueC=%d MinValueF=%d MaxValueF=%d Condition=%s", w.MinValueC, w.MaxValueC, w.MinValueF, w.MaxValueF, w.Condition)
 }
